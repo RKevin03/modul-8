03b95b6b002061413a0eae7d2df8e5964936fc68
RKevin03 <151544752+RKevin03@users.noreply.github.com>
2024-06-23 21:43:44 +0700

    Add files via upload

---
+++ storage/framework/views/d95f7dd2856135dd61ccc3538e49df69.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Controller & View pada Laravel</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
+</head>
+
+<body>
+    
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #644961">
+        <div class="container-fluid" style="background-color: #644961; ">
+            <ul class="navbar-nav mb-2 mb-md-0 fs-5" style="padding-right: 5rem;">
+                <li class="nav-item">
+                    <a class="nav-link text-light fw-semibold" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fw-semibold" href="#">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fw-semibold" href="<?php echo e(route('login')); ?>">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+<?php /**PATH C:\xampp\htdocs\authentication\resources\views/welcome.blade.php ENDPATH**/ ?>
